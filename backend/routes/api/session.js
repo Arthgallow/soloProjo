265d7e23c0d039a4750f18828438272e6c48c2bd
christopher <stopher.kirkum@gmail.com>
2021-06-18 08:42:04 -0700

    restore'd user session'

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -37,7 +37,24 @@ router.delete(
       res.clearCookie('token');
       return res.json({ message: 'success' });
     }
-  );
+);
+
+
+// Restore session user
+router.get(
+  '/',
+  restoreUser,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
+
+
 
 
 
